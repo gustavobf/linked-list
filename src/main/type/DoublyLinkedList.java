c1cb10aa04715fc24de50b4db3bef6b872e3e9a8
Gustavo Figueiredo <gustavobatistafig@hotmail.com>
2025-12-04 23:49:37 -0300

    Improve implementation for intuitive usage

---
+++ src/main/type/DoublyLinkedList.java
@@ -0,0 +1,79 @@
+package type;
+
+class Node<T> {
+    T value;
+    Node<T> prev;
+    Node<T> next;
+
+    Node (T value) {
+        this.value = value;
+        this.prev = this.next = null;
+    }
+
+}
+
+public class DoublyLinkedList<T> {
+    private int length;
+    private Node<T> head;
+    private Node<T> tail;
+
+    public DoublyLinkedList () {
+        this.length = 0;
+        this.head = this.tail = null;
+    }
+
+    public void append (T item) {
+        Node node = new Node(item);
+
+        if (this.head == null) {
+            this.length++;
+            this.head = this.tail = node;
+            return;
+        }
+
+        this.length++;
+
+        Node<T> curr = this.head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+
+        curr.next = this.tail = node;
+        node.prev = curr;
+
+    }
+
+    public void prepend (T item) {
+        Node node = new Node(item);
+
+        if (this.head == null) {
+            this.append(item);
+            return;
+        }
+
+        this.length++;
+
+        this.head.prev = node;
+        node.next = this.head;
+        this.head = node;
+    }
+
+    public int size () {
+        return this.length;
+    }
+
+    public T getFirst () {
+        if (this.head == null) {
+            return null;
+        }
+        return this.head.value;
+    }
+
+    public T getLast () {
+        if (this.tail == null) {
+            return null;
+        }
+        return this.tail.value;
+    }
+
+}
---
+++ src/test/type/DoublyLinkedListTest.java
@@ -0,0 +1,53 @@
+package type;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DoublyLinkedListTest {
+
+    @Test
+    public void testDoublyLinkedListCreation () {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        assertEquals(0, list.size());
+        assertNull(list.getFirst());
+        assertNull(list.getLast());
+    }
+
+    @Test
+    public void testDoublyLinkedListAppend () {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        list.append(10);
+        list.append(20);
+        list.append(30);
+        list.append(40);
+        list.append(50);
+        assertEquals(5, list.size());
+        assertEquals(10, list.getFirst());
+        assertEquals(50, list.getLast());
+    }
+
+    @Test
+    public void testDoublyLinkedListPrepend () {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        list.append(10);
+        list.append(20);
+        list.append(30);
+        list.append(40);
+        list.append(60);
+        list.prepend(-10);
+        assertEquals(6, list.size());
+        assertEquals(-10, list.getFirst());
+        assertEquals(60, list.getLast());
+    }
+
+    @Test
+    public void testDoublyLinkedListPrependCreation () {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        list.prepend(-5);
+        assertEquals(1, list.size());
+        assertEquals(-5, list.getFirst());
+        assertEquals(-5, list.getLast());
+    }
+
+}
